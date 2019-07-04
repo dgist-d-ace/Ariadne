9804b575f67e0498bdd6003881ea66a6b8b0b5ea
lcbk0321 <lcbk_0321@dgist.ac.kr>
2019-07-04 21:41:37 +0900

    change project configure windows->console

--- Ariadne/GeneratedFiles/ui_Ariadne.h
+++ Ariadne/GeneratedFiles/ui_Ariadne.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'Ariadne.ui'
 **
-** Created by: Qt User Interface Compiler version 5.12.2
+** Created by: Qt User Interface Compiler version 5.12.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -13,6 +13,7 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QComboBox>
 #include <QtWidgets/QGroupBox>
+#include <QtWidgets/QLCDNumber>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
@@ -43,6 +44,7 @@ public:
     QLineEdit *lineEdit_3;
     QLineEdit *lineEdit_4;
     QLineEdit *lineEdit_5;
+    QPushButton *pushButton_2;
     QGroupBox *groupBox_2;
     QGroupBox *groupBox_3;
     QPushButton *Btn_Mission0;
@@ -63,6 +65,12 @@ public:
     QPushButton *pushButton_12;
     QPushButton *pushButton_13;
     QSlider *verticalSlider;
+    QLabel *label_9;
+    QLabel *label_10;
+    QLineEdit *GearInput;
+    QLineEdit *SpeedInput;
+    QLabel *label_11;
+    QLineEdit *lineEdit_8;
     QWidget *tab_2;
     QTextBrowser *textBrowser;
     QPushButton *pushButton;
@@ -75,6 +83,14 @@ public:
     QLabel *label_7;
     QLabel *label;
     QLabel *label_8;
+    QLCDNumber *lcdNumber;
+    QLCDNumber *lcdNumber_2;
+    QLCDNumber *lcdNumber_3;
+    QLCDNumber *lcdNumber_4;
+    QLCDNumber *lcdNumber_5;
+    QLCDNumber *lcdNumber_6;
+    QLCDNumber *lcdNumber_7;
+    QTextBrowser *textBrowser_2;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -88,7 +104,7 @@ public:
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
         groupBox = new QGroupBox(centralWidget);
         groupBox->setObjectName(QString::fromUtf8("groupBox"));
-        groupBox->setGeometry(QRect(10, 180, 241, 171));
+        groupBox->setGeometry(QRect(10, 180, 241, 211));
         comboBox = new QComboBox(groupBox);
         comboBox->setObjectName(QString::fromUtf8("comboBox"));
         comboBox->setGeometry(QRect(120, 20, 94, 21));
@@ -119,45 +135,48 @@ public:
         lineEdit_5 = new QLineEdit(groupBox);
         lineEdit_5->setObjectName(QString::fromUtf8("lineEdit_5"));
         lineEdit_5->setGeometry(QRect(10, 140, 101, 21));
+        pushButton_2 = new QPushButton(groupBox);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(120, 170, 93, 28));
         groupBox_2 = new QGroupBox(centralWidget);
         groupBox_2->setObjectName(QString::fromUtf8("groupBox_2"));
         groupBox_2->setGeometry(QRect(10, 10, 241, 171));
         groupBox_3 = new QGroupBox(centralWidget);
         groupBox_3->setObjectName(QString::fromUtf8("groupBox_3"));
-        groupBox_3->setGeometry(QRect(10, 350, 241, 271));
+        groupBox_3->setGeometry(QRect(10, 390, 241, 241));
         Btn_Mission0 = new QPushButton(groupBox_3);
         Btn_Mission0->setObjectName(QString::fromUtf8("Btn_Mission0"));
-        Btn_Mission0->setGeometry(QRect(10, 20, 101, 41));
+        Btn_Mission0->setGeometry(QRect(10, 40, 101, 31));
         Btn_mission1 = new QPushButton(groupBox_3);
         Btn_mission1->setObjectName(QString::fromUtf8("Btn_mission1"));
-        Btn_mission1->setGeometry(QRect(10, 70, 101, 41));
+        Btn_mission1->setGeometry(QRect(10, 80, 101, 31));
         Btn_mission2 = new QPushButton(groupBox_3);
         Btn_mission2->setObjectName(QString::fromUtf8("Btn_mission2"));
-        Btn_mission2->setGeometry(QRect(10, 120, 101, 41));
+        Btn_mission2->setGeometry(QRect(10, 120, 101, 31));
         Btn_mission3 = new QPushButton(groupBox_3);
         Btn_mission3->setObjectName(QString::fromUtf8("Btn_mission3"));
-        Btn_mission3->setGeometry(QRect(10, 170, 101, 41));
+        Btn_mission3->setGeometry(QRect(10, 160, 101, 31));
         Btn_mission6 = new QPushButton(groupBox_3);
         Btn_mission6->setObjectName(QString::fromUtf8("Btn_mission6"));
-        Btn_mission6->setGeometry(QRect(130, 70, 101, 41));
+        Btn_mission6->setGeometry(QRect(130, 80, 101, 31));
         Btn_mission7 = new QPushButton(groupBox_3);
         Btn_mission7->setObjectName(QString::fromUtf8("Btn_mission7"));
-        Btn_mission7->setGeometry(QRect(130, 120, 101, 41));
+        Btn_mission7->setGeometry(QRect(130, 120, 101, 31));
         Btn_mission8 = new QPushButton(groupBox_3);
         Btn_mission8->setObjectName(QString::fromUtf8("Btn_mission8"));
-        Btn_mission8->setGeometry(QRect(130, 170, 101, 41));
+        Btn_mission8->setGeometry(QRect(130, 160, 101, 31));
         Btn_mission4 = new QPushButton(groupBox_3);
         Btn_mission4->setObjectName(QString::fromUtf8("Btn_mission4"));
-        Btn_mission4->setGeometry(QRect(10, 220, 101, 41));
+        Btn_mission4->setGeometry(QRect(10, 200, 101, 31));
         Btn_mission9 = new QPushButton(groupBox_3);
         Btn_mission9->setObjectName(QString::fromUtf8("Btn_mission9"));
-        Btn_mission9->setGeometry(QRect(130, 220, 101, 41));
+        Btn_mission9->setGeometry(QRect(130, 200, 101, 31));
         Btn_mission5 = new QPushButton(groupBox_3);
         Btn_mission5->setObjectName(QString::fromUtf8("Btn_mission5"));
-        Btn_mission5->setGeometry(QRect(130, 20, 101, 41));
+        Btn_mission5->setGeometry(QRect(130, 40, 101, 31));
         plainTextEdit = new QPlainTextEdit(centralWidget);
         plainTextEdit->setObjectName(QString::fromUtf8("plainTextEdit"));
-        plainTextEdit->setGeometry(QRect(10, 630, 1171, 161));
+        plainTextEdit->setGeometry(QRect(10, 640, 1171, 161));
         tabWidget = new QTabWidget(centralWidget);
         tabWidget->setObjectName(QString::fromUtf8("tabWidget"));
         tabWidget->setGeometry(QRect(970, 80, 211, 221));
@@ -165,20 +184,38 @@ public:
         tab->setObjectName(QString::fromUtf8("tab"));
         pushButton_10 = new QPushButton(tab);
         pushButton_10->setObjectName(QString::fromUtf8("pushButton_10"));
-        pushButton_10->setGeometry(QRect(80, 20, 51, 51));
+        pushButton_10->setGeometry(QRect(80, 10, 51, 51));
         pushButton_11 = new QPushButton(tab);
         pushButton_11->setObjectName(QString::fromUtf8("pushButton_11"));
-        pushButton_11->setGeometry(QRect(80, 80, 51, 51));
+        pushButton_11->setGeometry(QRect(80, 60, 51, 51));
         pushButton_12 = new QPushButton(tab);
         pushButton_12->setObjectName(QString::fromUtf8("pushButton_12"));
-        pushButton_12->setGeometry(QRect(140, 80, 51, 51));
+        pushButton_12->setGeometry(QRect(140, 60, 51, 51));
         pushButton_13 = new QPushButton(tab);
         pushButton_13->setObjectName(QString::fromUtf8("pushButton_13"));
-        pushButton_13->setGeometry(QRect(20, 80, 51, 51));
+        pushButton_13->setGeometry(QRect(20, 60, 51, 51));
         verticalSlider = new QSlider(tab);
         verticalSlider->setObjectName(QString::fromUtf8("verticalSlider"));
-        verticalSlider->setGeometry(QRect(160, 20, 22, 51));
+        verticalSlider->setGeometry(QRect(160, 10, 22, 41));
         verticalSlider->setOrientation(Qt::Vertical);
+        label_9 = new QLabel(tab);
+        label_9->setObjectName(QString::fromUtf8("label_9"));
+        label_9->setGeometry(QRect(20, 120, 64, 15));
+        label_10 = new QLabel(tab);
+        label_10->setObjectName(QString::fromUtf8("label_10"));
+        label_10->setGeometry(QRect(10, 140, 64, 15));
+        GearInput = new QLineEdit(tab);
+        GearInput->setObjectName(QString::fromUtf8("GearInput"));
+        GearInput->setGeometry(QRect(80, 120, 113, 21));
+        SpeedInput = new QLineEdit(tab);
+        SpeedInput->setObjectName(QString::fromUtf8("SpeedInput"));
+        SpeedInput->setGeometry(QRect(80, 140, 113, 21));
+        label_11 = new QLabel(tab);
+        label_11->setObjectName(QString::fromUtf8("label_11"));
+        label_11->setGeometry(QRect(10, 170, 64, 15));
+        lineEdit_8 = new QLineEdit(tab);
+        lineEdit_8->setObjectName(QString::fromUtf8("lineEdit_8"));
+        lineEdit_8->setGeometry(QRect(80, 170, 113, 21));
         tabWidget->addTab(tab, QString());
         tab_2 = new QWidget();
         tab_2->setObjectName(QString::fromUtf8("tab_2"));
@@ -216,6 +253,30 @@ public:
         label_8 = new QLabel(groupBox_4);
         label_8->setObjectName(QString::fromUtf8("label_8"));
         label_8->setGeometry(QRect(20, 120, 64, 15));
+        lcdNumber = new QLCDNumber(groupBox_4);
+        lcdNumber->setObjectName(QString::fromUtf8("lcdNumber"));
+        lcdNumber->setGeometry(QRect(130, 30, 64, 23));
+        lcdNumber_2 = new QLCDNumber(groupBox_4);
+        lcdNumber_2->setObjectName(QString::fromUtf8("lcdNumber_2"));
+        lcdNumber_2->setGeometry(QRect(130, 60, 64, 23));
+        lcdNumber_3 = new QLCDNumber(groupBox_4);
+        lcdNumber_3->setObjectName(QString::fromUtf8("lcdNumber_3"));
+        lcdNumber_3->setGeometry(QRect(130, 90, 64, 23));
+        lcdNumber_4 = new QLCDNumber(groupBox_4);
+        lcdNumber_4->setObjectName(QString::fromUtf8("lcdNumber_4"));
+        lcdNumber_4->setGeometry(QRect(130, 120, 64, 23));
+        lcdNumber_5 = new QLCDNumber(groupBox_4);
+        lcdNumber_5->setObjectName(QString::fromUtf8("lcdNumber_5"));
+        lcdNumber_5->setGeometry(QRect(130, 150, 64, 23));
+        lcdNumber_6 = new QLCDNumber(groupBox_4);
+        lcdNumber_6->setObjectName(QString::fromUtf8("lcdNumber_6"));
+        lcdNumber_6->setGeometry(QRect(130, 180, 64, 23));
+        lcdNumber_7 = new QLCDNumber(groupBox_4);
+        lcdNumber_7->setObjectName(QString::fromUtf8("lcdNumber_7"));
+        lcdNumber_7->setGeometry(QRect(130, 210, 64, 23));
+        textBrowser_2 = new QTextBrowser(groupBox_4);
+        textBrowser_2->setObjectName(QString::fromUtf8("textBrowser_2"));
+        textBrowser_2->setGeometry(QRect(130, 240, 61, 21));
         AriadneClass->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(AriadneClass);
         menuBar->setObjectName(QString::fromUtf8("menuBar"));
@@ -245,6 +306,7 @@ public:
         lineEdit_3->setText(QApplication::translate("AriadneClass", "Camera2", nullptr));
         lineEdit_4->setText(QApplication::translate("AriadneClass", "LiDAR", nullptr));
         lineEdit_5->setText(QApplication::translate("AriadneClass", "GPS", nullptr));
+        pushButton_2->setText(QApplication::translate("AriadneClass", "\355\231\225\354\235\270", nullptr));
         groupBox_2->setTitle(QApplication::translate("AriadneClass", "GroupBox", nullptr));
         groupBox_3->setTitle(QApplication::translate("AriadneClass", "Mission", nullptr));
         Btn_Mission0->setText(QApplication::translate("AriadneClass", "Mission0", nullptr));
@@ -261,6 +323,9 @@ public:
         pushButton_11->setText(QApplication::translate("AriadneClass", "\342\226\274", nullptr));
         pushButton_12->setText(QApplication::translate("AriadneClass", "\342\226\266", nullptr));
         pushButton_13->setText(QApplication::translate("AriadneClass", "\342\227\200", nullptr));
+        label_9->setText(QApplication::translate("AriadneClass", "GEAR", nullptr));
+        label_10->setText(QApplication::translate("AriadneClass", "SPEED", nullptr));
+        label_11->setText(QApplication::translate("AriadneClass", "STEER", nullptr));
         tabWidget->setTabText(tabWidget->indexOf(tab), QApplication::translate("AriadneClass", "ManualMode", nullptr));
         textBrowser->setHtml(QApplication::translate("AriadneClass", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
